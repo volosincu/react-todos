36f34a74045bb7a6eb3492c4439e69301129d66a
volosincu <bogdan.volosincu@gmail.com>
2019-04-19 22:19:09 +0300

    update failing test, add error validation on reminder text >= 30 chars

--- src/components/day/__tests__/day.test.js
+++ src/components/day/__tests__/day.test.js
@@ -5,7 +5,9 @@ import { shallow, mount, render } from 'enzyme';
 
 test('Expect to have a valid wrapper after Day load.', () => {
 
-    const enzyWrapper = shallow(<Day />);
+    const Props = { day: "1", reminders: [] };
+
+    const enzyWrapper = shallow(<Day {...Props} />);
     /*console.log(enzyWrapper.debug());*/
 
     expect(enzyWrapper).toBeTruthy();
@@ -13,7 +15,7 @@ test('Expect to have a valid wrapper after Day load.', () => {
 
 test('Expect to find the corect content passed trought props rendered.', () => {
 
-    const Props = { day: "1" };
+    const Props = { day: "1", reminders: [] };
 
     const enzyWrapper = mount(<Day {...Props} />);
     const elemQuery = enzyWrapper.find(".day-component .day");
@@ -28,7 +30,7 @@ test('Expect to find the corect content passed trought props rendered.', () => {
 describe("Test if component has the expected functionalities. ", ()=>{
 
     it('Expect to find the add reminder button.', () => {
-        const Props = { day: "1", reminders: null };
+        const Props = { day: "1", reminders: [] };
         const enzyWrapper = mount(<Day {...Props} />);
         const remindersCounter = enzyWrapper.find(".day-component .reminders-add");
 
@@ -41,11 +43,7 @@ describe("Test component behaviour when reminders are present or not. ", ()=>{
 
         const Props = { day: "1", reminders: [
             { text: "clean room",
-              year: 2019,
-              month: 4,
-              day: 2,
-              hour: 10,
-              min: 33 }] };
+              date: "2019-06-11"}] };
 
         const enzyWrapper = mount(<Day {...Props} />);
         const remindersCounter = enzyWrapper.find(".day-component .reminders-counter");
@@ -55,14 +53,12 @@ describe("Test component behaviour when reminders are present or not. ", ()=>{
 
     it('Expect to find the reminders counter.', () => {
 
-        const Props = { day: "1", reminders: null };
+        const Props = { day: "1", reminders: [] };
 
         const enzyWrapper = mount(<Day {...Props} />);
         const remindersCounter = enzyWrapper.find(".day-component .reminders-counter");
 
         expect(remindersCounter).toHaveLength(0);
-
     });
-
 
 });
--- src/components/day/day.js
+++ src/components/day/day.js
@@ -2,21 +2,40 @@ import React, { useState, useEffect } from 'react';
 import dayStyle from './day.css';
 
 /**
+ * @TODO move in separate module
+ * @description set the day
+ */
+const createReminderDate = (day) => {
+    if(day == null){
+        throw new Error("set the day.");
+    }
+
+    const date = new Date(),
+          m = date.getUTCMonth(),
+          y = date.getUTCFullYear();
+    return new Date(y, m, day+1).toISOString().substr(0, 10);;
+};
+
+/**
  * @desc Component to render a individual day of calendar
  */
 function Day ({day, reminders}) {
 
     const [data, setData] = useState({reminders: reminders, error: ""});
     const [newReminder, setNewReminder] = useState("");
-    const [newReminderDate, setNewReminderDate] = useState(new Date().toISOString().substr(0, 10));
-    const [newError, setError] = useState(false);
-    const [popup, setPopup] = useState("");
+    const [newReminderDate, setNewReminderDate] = useState(createReminderDate(day));
+    const [error, setError] = useState("");
+    const [popup, setPopup] = useState({create: "", show:""});
 
-    const createRemindersNotification = (counter)=>{
+    const createRemindersNotification = (counter, handler)=>{
         const remindersNotificationClass = counter > 0 ? "reminders-counter" : "no-reminders";
-        return <div className={remindersNotificationClass}>{counter}</div>;
+        return <div className={remindersNotificationClass} onClick={handler}>{counter}</div>;
     };
 
+    /**
+     *
+     * @description check if reminders are defined and get the length otherwise return 0
+     */
     const countReminders = (dayReminders)=> {
         let size = 0;
         if(dayReminders != null) {
@@ -26,62 +45,93 @@ function Day ({day, reminders}) {
         return size;
     };
 
+    /**
+     *
+     * @description save reminder handler and close popups
+     */
     const addReminderHandler = ()=> {
         setData({reminders: data.reminders.concat([{text: newReminder, date: newReminderDate}])});
-        setPopup("");
 
         const date = new Date().toISOString().substr(0, 10);
         setNewReminderDate(date);
         setNewReminder("");
+
+        setPopup({create: "", show:""});
     };
 
+    /**
+     *
+     * @description open add reminder popup
+     */
     const openAddReminderHandler = ()=> {
-        setPopup("reminder-popup-open");
+        setPopup({create: "reminder-popup-open", show:""});
     };
 
+    /**
+     *
+     * @description open show reminders of day popup
+     */
+    const openShowReminderHandler = ()=> {
+        setPopup({create: "", show:"reminder-popup-open"});
+    };
+
+    /**
+     *
+     * @description close popups
+     */
     const cancelReminderHandler = ()=> {
-        setPopup("");
+        setPopup({create: "", show:""});
     };
 
-
+    /**
+     *
+     * @description handler for reading the reminder text input by user
+     */
     const getReminderText = (e) => {
         const val = e.target.value;
 
-        if(val.length > 30){
-            setError(true);
+        if(val.length >= 30){
+            setError("Error. The limit is 30 characters.");
         }else {
-            setError(false);
+            setError("");
             setNewReminder(val);
         }
     };
 
-    const getReminderTime = (e) => {
-        const val = e.target.value;
-        const date = new Date(val).toISOString().substr(0, 10);
-
-        setNewReminderDate(date);
-    };
-
     const remindersCounter = countReminders(data.reminders);
-    const notification = createRemindersNotification(remindersCounter);
+    const notification = createRemindersNotification(remindersCounter, openShowReminderHandler);
+
+    const {show, create} = popup,
+          dayReminders = data.reminders.map((r, i)=>{
+              return <li key={i}>{r.text}</li>;
+          });
 
     return (
             <div className="day-component">
-            <input className="reminders-add" type="button" value="+" onClick={openAddReminderHandler}></input>
-            {notification}
-            <div className="day">{day}</div>
-            <div>
-            <br/>
-            <div className={`reminder-popup ${popup} create-reminder`}>
-            <div className="popup-content">
-            <input type="text" onChange={getReminderText} step="1"/>
-            <input id="today" type="date" step="2" value={newReminderDate} onChange={getReminderTime}/>
+                <input className="reminders-add" type="button" value="+" onClick={openAddReminderHandler}></input>
+                {notification}
+                <div className="day">{day}</div><div>
+                <br/>
 
-            <button onClick={addReminderHandler}>Save</button>
-            <button onClick={cancelReminderHandler}>Cancel</button>
-            </div>
+                <div className={`reminder-popup ${create} create-reminder`}>
+                    <div className="popup-content">
+                        <input type="text" onChange={getReminderText} step="1"/>
+                        <div>{error}</div>
+                        <input id="today" disabled="disabled" type="date" step="2" value={newReminderDate} />
 
-        </div>
+                        <button onClick={addReminderHandler}>Save</button>
+                        <button onClick={cancelReminderHandler}>Cancel</button>
+                    </div>
+               </div>
+
+               <div className={`reminder-popup ${show} show-reminders`}>
+                   <div className="popup-content">
+                       <button onClick={cancelReminderHandler}>Close</button>
+                       <ul>
+                           {dayReminders}
+                       </ul>
+                   </div>
+                </div>
             </div>
         </div>
     );
